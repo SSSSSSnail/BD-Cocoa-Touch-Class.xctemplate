f7e7c7122f45cd401915c41d4b91ead3f7694ee7
Snail <warlocke@163.com>
2015-01-19 16:28:13 +0800

    BD Cocoa Touch Class.xctemplate

---
+++ UICollectionReusableViewObjective-C/___FILEBASENAME___.m
@@ -0,0 +1,35 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAME___.h"
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+#pragma mark - Life Cycle
+//生命周期相关方法: -init -dealloc etc.
+
+#pragma mark - Public Method
+//在头文件声明的公开方法
+#pragma mark 功能分类
+//功能分类不要添加 -
+
+#pragma mark - Private Method
+//私有方法: 命名以_开始
+
+#pragma mark - Protocol Implementation
+//协议实现方法
+
+#pragma mark - Override Method
+//复写基类方法
+
+#pragma mark - Notification Observer
+//通知响应方法
+
+#pragma mark - Delegate Implementation
+//代理实现方法
+
+@end
